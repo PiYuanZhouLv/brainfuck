44c35eaf3536227d0f52add2fec8778e80867d85
PiYuanZhouLv <18379028718@139.com>
2022-01-08 11:51:41 +0800

    update setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='brainfuck',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/PiYuanZhouLv/brainfuck/',
+    license='',
+    author='PiYuanZhouLv',
+    author_email='18379028718@139.com',
+    description='A Brainf*ck compiler in Python.'
+)
